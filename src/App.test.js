b72d59a806f7c98e6ce9bdb427de996389626af3
Rishab Surana <er.rishab11@gmail.com>
2021-09-21 19:40:04 +0530

    fixed import

--- src/App.test.js
+++ src/App.test.js
@@ -1,7 +1,8 @@
 import { configure } from "enzyme";
 import Adapter from "enzyme-adapter-react-16";
-configure({ adapter: new Adapter() });
 import App from './App';
+configure({ adapter: new Adapter() });
+
 
 it("renders without crashing", () => {
 });
